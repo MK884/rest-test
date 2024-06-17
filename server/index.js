c6e40bd842d742f064d881254e2fac494f623e31
MK884 <marchantkhalid7810@gmail.com>
2024-06-17 19:09:32 +0530

    Connected with mongodb

--- server/index.js
+++ server/index.js
@@ -1,75 +1,140 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const Note = require("./models/note");
 
 const app = express();
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+
+// const password = process.argv[2]
+
+// // DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+// const url =
+//   `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+
+// mongoose.set('strictQuery',false)
+// mongoose.connect(url)
+
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,
+// })
+
+// const Note = mongoose.model('Note', noteSchema)
+
+// noteSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+
+// let notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only JavaScript",
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true,
+//   },
+// ];
+
+// const generateId = () => {
+//   const maxId = Note.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+//   return maxId + 1;
+// };
 
 app.get("/", (req, res) => {
   res.send("<h1>Wlecome</h1>");
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  let id = Number(req.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (!note) return res.status(404).end();
-  res.json(note);
-});
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  
+  next(error)
+}
 
-app.get("/api/notes/:id", (req, res) => {
-  let id = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== id);
+app.use(errorHandler)
 
-  res.status(200).end();
-});
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
 
-app.post("/api/notes", (req, res) => {
-  let body = req.body;
-
-  if(!body){
-    return res.status(400).json({
-        'error': 'content missing'
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
     })
-  }
+
+    .catch(error => {
+      // console.log(error)
+      // response.status(400).send({ error: 'malformatted id' })
+      next(error);
+    })
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
   const note = {
-    id: generateId(),
     content: body.content,
-    important: Boolean(body.important) || false
-  };
+    important: body.important,
+  }
 
-  notes = notes.concat(note);
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
 
-  res.json(note);
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
