c5d3c685825e528abf6cc0a7bb879c5a97a47558
MK884 <marchantkhalid7810@gmail.com>
2024-06-20 18:42:20 +0530

    add: login form in frontend

--- client/src/services/notes.js
+++ client/src/services/notes.js
@@ -1,20 +1,30 @@
-import axios, { all } from 'axios';
+import axios, { all } from "axios";
 
-const baseURL = '/api/notes'
+const baseURL = "/api/notes";
 
-const getAll = ()=>{
-    const request = axios.get(baseURL);
-    return request.then(res=>res.data);
-}
+let token = null;
 
-const create = newObject => {
-    const request = axios.post(baseURL,newObject);
-    return request.then(res=>res.data);
-}
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
 
-const update = (id, newObject) =>{
-    const request = axios.put(`${baseURL}/${id}`, newObject)
-    return request.then(response => response.data)
-} 
+const getAll = () => {
+  const request = axios.get(baseURL);
+  return request.then((res) => res.data);
+};
 
-export default { getAll, create, update}
+const create = async (newObject) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+
+  const response = await axios.post(baseURL, newObject, config);
+  return response.data;
+};
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseURL}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
+
+export default { getAll, create, update, setToken };
